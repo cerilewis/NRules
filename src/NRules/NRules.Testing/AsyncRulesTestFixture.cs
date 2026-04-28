643b41a2f683be947dd22aabaa8f5fc77a280f03
Ceri Lewis <ceri.lewis@gmail.com>
2026-04-28 21:49:24 +0200

    Add async test fixtures and harness for rule validation

---
+++ src/NRules/NRules.Testing/AsyncRulesTestFixture.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace NRules.Testing;
+
+/// <summary>
+/// Fixture to test rules using an async session.
+/// </summary>
+public class AsyncRulesTestFixture
+{
+    private readonly Lazy<AsyncRulesTestHarness> _testHarness;
+    private IRuleAsserter? _asserter;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AsyncRulesTestFixture"/> class.
+    /// </summary>
+    public AsyncRulesTestFixture()
+    {
+        Setup = new RulesTestSetup();
+        _testHarness = new Lazy<AsyncRulesTestHarness>(() => new AsyncRulesTestHarness(Setup));
+    }
+
+    /// <summary>
+    /// Gets or sets the framework-specific asserter to validate rule firing expectations.
+    /// </summary>
+    /// <remarks>If asserter is not set, it's initialized to a <see cref="RuleAsserter"/> on first access.</remarks>
+    public IRuleAsserter Asserter
+    {
+        get => _asserter ??= new RuleAsserter();
+        set => _asserter = value;
+    }
+
+    /// <summary>
+    /// Gets the test setup to register rules under test.
+    /// </summary>
+    public IRulesTestSetup Setup { get; }
+
+    /// <summary>
+    /// Gets the current async rules engine session.
+    /// </summary>
+    public IAsyncSession Session => _testHarness.Value.Session;
+
+    /// <summary>
+    /// Gets the rule invocation recorder to inspect rule firing.
+    /// </summary>
+    public IRuleInvocationRecorder Recorder => _testHarness.Value.Recorder;
+
+    /// <summary>
+    /// Configures assertions for rules firing using an expectation builder.
+    /// </summary>
+    public void Verify(Action<IRulesFiringVerification> buildAction)
+    {
+        var verification = _testHarness.Value.GetRulesVerification();
+        var result = verification.Verify(buildAction);
+        Asserter.Assert(result);
+    }
+
+    /// <summary>
+    /// Configures assertions of the exact rules firing sequence using an expectation builder.
+    /// </summary>
+    public void VerifySequence(Action<IRuleSequenceFiringVerification> buildAction)
+    {
+        var verification = _testHarness.Value.GetRulesVerification();
+        var result = verification.VerifySequence(buildAction);
+        Asserter.Assert(result);
+    }
+}
---
+++ src/NRules/NRules.Testing/AsyncRulesTestHarness.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+
+namespace NRules.Testing;
+
+/// <summary>
+/// Rules under test compiled into an async rules engine session along with the means to verify rules firing.
+/// </summary>
+public class AsyncRulesTestHarness
+{
+    private readonly RuleInvocationRecorder _invocationRecorder;
+    private readonly RulesUnderTest _ruleSet;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AsyncRulesTestHarness"/> class.
+    /// </summary>
+    /// <param name="setup">Rules test setup information.</param>
+    public AsyncRulesTestHarness(IRulesTestSetup setup)
+    {
+        _ruleSet = new RulesUnderTest(setup.Rules);
+
+        var compiler = new RuleCompiler();
+        setup.CompilerSetupAction?.Invoke(compiler);
+
+        var ruleDefinitions = setup.Rules.Select(x => x.Definition);
+        var factory = compiler.CompileAsync(ruleDefinitions);
+        Session = factory.CreateSession();
+        
+        _invocationRecorder = new RuleInvocationRecorder(Session);
+    }
+
+    /// <summary>
+    /// Gets the rules under test.
+    /// </summary>
+    public IRulesUnderTest RuleSet => _ruleSet;
+
+    /// <summary>
+    /// Gets the current async rules engine session.
+    /// </summary>
+    public IAsyncSession Session { get; }
+
+    /// <summary>
+    /// Gets the rule invocation recorder to control and inspect rules firing.
+    /// </summary>
+    public IRuleInvocationRecorder Recorder => _invocationRecorder;
+
+    /// <summary>
+    /// Creates a rules verification builder to check rules firing expectations.
+    /// </summary>
+    /// <returns>Instance of the verification builder.</returns>
+    public IRulesVerification GetRulesVerification()
+    {
+        return new RulesVerification(_ruleSet, Recorder.GetInvocations());
+    }
+}
